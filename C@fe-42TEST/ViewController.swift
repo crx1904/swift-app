d4a72bc13f29618fc4640b4c485f08cf2434572b
Mark Heitfeldt <cruxis0201@yahoo.de>
2019-09-03 18:01:44 +0200

    My first commit

---
+++ C@fe-42TEST/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  C@fe-42TEST
+//
+//  Created by Mark Heitfeldt on 02.09.19.
+//  Copyright © 2019 Mark Heitfeldt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount += 1
+        print("Button tapped")
+        
+        if tapCount >= 10 {
+            theLabel.text = "You've pushed the button 10 times!"
+        }
+    
+        
+    }
+    
+    @IBAction func button2Tapped(_ sender: Any) {
+    
+        theLabel.text = "Buttons are cool!"
+        print("Button tapped")
+    
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
